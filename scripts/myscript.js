6f77704cb2bea5b8be9c845d84f5ca642b1fc056
Alex <lathamwyse123@gmail.com>
2022-11-25 13:00:28 -0800

    added function to hide text area and save/cancel buttons associated with cancel button

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -5,26 +5,29 @@ const Button = document.querySelectorAll("button");
 const asideTheme = document.querySelector("aside");
 const infoButtons = document.querySelectorAll(".aboutdoc");
 const NoteCreate = document.querySelector("#newnote");
+const textArea = document.querySelector("textarea");
 
-
-
-
-let NoteSet = []
+let NoteSet = [];
 
 
 function DarkTheme() {
-    Button[2].classList.toggle("darkthemebutton")
-    Button[3].classList.toggle("darkthemebutton")
-    Button[4].classList.toggle("darkthemebutton")
+    Button[2].classList.toggle("darkthemecancel");
+    Button[3].classList.toggle("darkthemecancel");
+    Button[4].classList.toggle("darkthemecancel");
     DarkBody.classList.toggle("darkthemebody");
     cancelButton.classList.toggle("darkthemecancel");
     asideTheme.classList.toggle("darkthemeaside");
     infoButtons[0].classList.toggle("darkthemebody");
     infoButtons[1].classList.toggle("darkthemebody");
+    textArea.classList.toggle("darkthemebutton");
 };
 
 darkThemeContainer.addEventListener("click", DarkTheme);
 
-function createNote() {
-
+function display() {
+    textArea.classList.add("hidden");
+    cancelButton.classList.add("hidden");
+    Button[4].classList.add("hidden");
 }
+
+cancelButton.addEventListener("click", display);
