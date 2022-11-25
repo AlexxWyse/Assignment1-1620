0eb454b9555a144b91ebaeaa6c376259a898603e
Alex <lathamwyse123@gmail.com>
2022-11-25 12:43:12 -0800

    finished dark theme for js

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,16 +1,30 @@
 const DarkBody = document.body;
 const darkThemeContainer = document.querySelector(".darktheme");
 const cancelButton = document.querySelector(".cancel");
-const saveButton = document.querySelector("button");
+const Button = document.querySelectorAll("button");
 const asideTheme = document.querySelector("aside");
+const infoButtons = document.querySelectorAll(".aboutdoc");
+const NoteCreate = document.querySelector("#newnote");
 
 
 
-let DarkTheme = () => {
+
+let NoteSet = []
+
+
+function DarkTheme() {
+    Button[2].classList.toggle("darkthemebutton")
+    Button[3].classList.toggle("darkthemebutton")
+    Button[4].classList.toggle("darkthemebutton")
     DarkBody.classList.toggle("darkthemebody");
     cancelButton.classList.toggle("darkthemecancel");
-    saveButton.classList.toggle("darkthemebutton");
     asideTheme.classList.toggle("darkthemeaside");
+    infoButtons[0].classList.toggle("darkthemebody");
+    infoButtons[1].classList.toggle("darkthemebody");
 };
 
 darkThemeContainer.addEventListener("click", DarkTheme);
+
+function createNote() {
+
+}
